cad94d90fb291e3d2ab8738e193bc80ee60f9c35
Caio-Kaefer <kaefer.caio@gmail.com>
2024-09-16 20:11:11 -0300

    logica de escudos individuais implementada e svg correto sendo utilizado

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const apiUrl = 'https://sevn-pleno-esportes.deno.dev/';
 
+    const teamGradients = {
+        'time-a': ['#FF5733', '#FFBD33'],
+        'time-b': ['#33FF57', '#33FFBD'],
+        'time-c': ['#3357FF', '#33A1FF'],
+        'time-d': ['#FF33A1', '#FF33D4'],
+        'time-e': ['#A133FF', '#D433FF'],
+        'time-f': ['#33FFA1', '#33FFD4'],
+        'time-g': ['#FFA133', '#FFD433'],
+        'time-h': ['#A1FF33', '#D4FF33']
+    };
+
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
@@ -28,14 +39,13 @@ document.addEventListener('DOMContentLoaded', () => {
             gameElement.classList.add('game');
             gameElement.innerHTML = `
                 <div class="team">
-                ${getShieldIcon()}
-                ${game.team_home_name}    
-                
+                    ${getShieldIcon(game.team_home_id)}
+                    ${game.team_home_name}
                 </div>
-                <div class="score">${game.team_home_score} - ${game.team_away_score}</div>
+                <div class="score">${game.team_home_score}<div class="separator">X</div>${game.team_away_score}</div>
                 <div class="team">
                     ${game.team_away_name}
-                    ${getShieldIcon()}
+                    ${getShieldIcon(game.team_away_id)}
                 </div>
             `;
 
@@ -65,17 +75,18 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    function getShieldIcon() {
+    function getShieldIcon(teamId) {
+        const gradientColors = teamGradients[teamId] || [getRandomColor(), getRandomColor()];
         const gradientId = `gradient-${Math.random().toString(36).substr(2, 9)}`;
         return `
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="url(#${gradientId})">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 40" fill="url(#${gradientId})">
                 <defs>
                     <linearGradient id="${gradientId}" x1="0%" y1="0%" x2="100%" y2="100%">
-                        <stop offset="0%" style="stop-color:${getRandomColor()};stop-opacity:1" />
-                        <stop offset="100%" style="stop-color:${getRandomColor()};stop-opacity:1" />
+                        <stop offset="0%" style="stop-color:${gradientColors[0]};stop-opacity:1" />
+                        <stop offset="100%" style="stop-color:${gradientColors[1]};stop-opacity:1" />
                     </linearGradient>
                 </defs>
-                <path d="M12 2L2 7v6c0 5.25 3.75 10 10 10s10-4.75 10-10V7L12 2z"/>
+                <path d="M16 0C14.8835 1.49628 14.1602 4.31649 9.54496 5.01371C9.11253 5.07638 8.70368 5.10772 8.31056 5.10772C5.38575 5.10772 3.61671 3.51743 3.61671 3.51743L0 7.31688C0 7.31688 5.59803 9.10302 1.11646 25.1156C-1.91843 35.9577 14.5926 37.6028 16 40C17.3995 37.6028 33.9106 35.9577 30.8835 25.1156C26.4098 9.10302 32 7.31688 32 7.31688L28.3754 3.51743C28.3754 3.51743 26.6064 5.10772 23.6816 5.10772C23.2885 5.10772 22.8796 5.07638 22.4472 5.01371C17.8398 4.32432 17.1165 1.49628 15.9921 0L16 0Z"/>
             </svg>
         `;
     }
